a7ea0625eaeb7842a277f244426a78873116d12f
North-West-Wind <wsyn148@gmail.com>
2025-04-17 22:02:09 +0800

    better styling

--- client/handlers/resize.ts
+++ client/handlers/resize.ts
@@ -2,9 +2,17 @@ const resize = () => {
 	if (window.innerHeight > window.innerWidth) {
 		// vertical
 		document.body.style.flexDirection = "column";
+		Array.from(document.getElementsByClassName("column")).forEach(element => {
+			(element as HTMLElement).classList.remove("hori");
+			(element as HTMLElement).classList.add("vert");
+		});
 	} else {
 		// horizontal
 		document.body.style.flexDirection = "row";
+		Array.from(document.getElementsByClassName("column")).forEach(element => {
+			(element as HTMLElement).classList.remove("vert");
+			(element as HTMLElement).classList.add("hori");
+		});
 	}
 };
 
